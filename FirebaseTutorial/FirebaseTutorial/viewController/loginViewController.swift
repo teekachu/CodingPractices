5935c17fe57c6e1bf83a2faf3421162f5400bbfb
Tee Becker <teebecker@Tees-MacBook.local>
2020-10-12 08:41:57 -0700

    added finished product of FIREBASE sign in page tutorial

--- FirebaseTutorial/FirebaseTutorial/viewController/loginViewController.swift
+++ FirebaseTutorial/FirebaseTutorial/viewController/loginViewController.swift
@@ -6,22 +6,20 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class loginViewController: UIViewController {
-
-    @IBOutlet weak var firstNameTextField: UITextField!
-    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passWordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var errorLabel: UILabel!
     
-    @IBAction func loginTapped(_ sender: Any) {
-        
-    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setUpElements()
         // Do any additional setup after loading the view.
     }
@@ -32,19 +30,76 @@ class loginViewController: UIViewController {
         errorLabel.alpha = 0
         
         // style elements
-        Utilities.styleTextField(firstNameTextField)
-        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passWordTextField)
         Utilities.styleHollowButton(loginButton)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func validateFields() -> String? {
+        
+        // series of checks to return error msg
+        // check if any of the fields are empty
+        if emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || passWordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return " please fill in all fields "
+        }
+        
+        return nil
     }
-    */
-
+    
+    // MARK: Login button
+    
+    @IBAction func loginTapped(_ sender: Any) {
+        // validate textfield ( all filled in )
+        let error = validateFields()
+        if error != nil{
+            showError(message: error!)
+        } else{
+            // signing in the user
+            signIn()
+            
+        }
+        
+    }
+    
+    func showError(message: String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
+    func signIn(){
+        //
+        //create clean version of email and pass
+        let emailAddress = self.emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let passWord = self.passWordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // auth per firebase documentation
+        Auth.auth().signIn(withEmail: emailAddress, password: passWord) {[weak self] (authResult, error) in
+            
+            if error != nil{
+                // could not sign in
+                self?.errorLabel.text = error?.localizedDescription
+                self?.errorLabel.alpha = 1
+                
+            } else{
+                // no error, sign in successfully
+                let homeVC = self?.storyboard?.instantiateViewController(withIdentifier: "homeVC") as? homeViewController
+                self?.view.window?.rootViewController = homeVC
+                self?.view.window?.makeKeyAndVisible()
+                
+            }
+            
+        }
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- FirebaseTutorial/FirebaseTutorial/viewController/signUpViewController.swift
+++ FirebaseTutorial/FirebaseTutorial/viewController/signUpViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 class signUpViewController: UIViewController {
     
@@ -50,8 +52,14 @@ class signUpViewController: UIViewController {
             showError(message: error!)
         } else{
             // no error
+            // create cleaned versions of data
+            let firstName = self.firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = self.lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let emailAddress = self.emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let passWord = self.passwordTextfield.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
             // create the user and transition to home screen
-            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextfield.text!) { (successResult, error) in
+            Auth.auth().createUser(withEmail: emailAddress, password: passWord) { (successResult, error) in
                 
                 // check for errors:
                 if error != nil {
@@ -62,58 +70,85 @@ class signUpViewController: UIViewController {
                 } else{
                     
                     // user was created successfully , store firstname / lastname
+                    let db = Firestore.firestore()
+                    db.collection("users").addDocument(data: [
+                        "firstName": firstName,
+                        "lastName": lastName,
+
+                        
+                    ]) { (error) in
+                        
+                        if error != nil{
+                            // there is an error
+                            self.showError(message: "User data couldn't be saved on database side")
+                            
+                        }
+                    }
                     
+                    // transition to Home screen
+                    
+                    self.transitionToHome()
                 }
             }
             
-        
-    }
-    
-    
-    
-    
-    
-}
-
-// check fields and validate the data is correct. If all correct, return nil, else return error message
-func validateFields() -> String? {
-    
-    // series of checks to return error msg
-    // check if any of the fields are empty
-    if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-        emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-        passwordTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
-        
-        return " please fill in all fields "
-    }
-    
-    // check if pswd is secure. Use code in Utilities
-    
-    if let cleanPassword = passwordTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines){
-        
-        if Utilities.isPasswordValid(cleanPassword) == false{
-            // password isn't secure enough
-            return " Please make sure to include special characters and numbers in your password"
+            
         }
+        
+        
+        
+        
+        
     }
     
+    // check fields and validate the data is correct. If all correct, return nil, else return error message
+    func validateFields() -> String? {
+        
+        // series of checks to return error msg
+        // check if any of the fields are empty
+        if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return " please fill in all fields "
+        }
+        
+        // check if pswd is secure. Use code in Utilities
+        
+        if let cleanPassword = passwordTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines){
+            
+            if Utilities.isPasswordValid(cleanPassword) == false{
+                // password isn't secure enough
+                return " Please make sure to include special characters and numbers in your password"
+            }
+        }
+        
+        
+        return nil // everything is fine
+    }
     
-    return nil // everything is fine
-}
+    func showError(message: String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
+    func transitionToHome(){
+        
+        let homeVC = storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.homeViewController) as? homeViewController
+        // made the homeVC the rootController
+        view.window?.rootViewController = homeVC
+        // make it the key window and visible
+        view.window?.makeKeyAndVisible()
 
-func showError(message: String) {
-    errorLabel.text = message
-    errorLabel.alpha = 1
-}
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destination.
- // Pass the selected object to the new view controller.
- }
- */
-
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
